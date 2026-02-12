0bbe46693b2368cd8d8798c3fcce3ae49eb98f05
Fernando Tadeo <fernandotadeos@gmail.com>
2026-02-11 23:26:40 -0300

    Fix layout no translate

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en" className="dark" suppressHydrationWarning>
+    <html lang="es" translate="no" className="dark" suppressHydrationWarning>
       <head>
         <style>{`
 html {
